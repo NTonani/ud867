d067bd1f71be035c27d01954ed7e8ecd5f032055
Nathan Tonani <nathantonani@gmail.com>
2017-02-06 13:45:02 -0800

    androidTest dir setup

---
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,17 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by ntonani on 2/6/17.
+ */
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest {
+    @Test
+    public void testJokesEndpoint() throws Exception {
+
+    }
+}
