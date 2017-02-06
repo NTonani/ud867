3424a7cd2ad4ef723c1d2528db1ef3b90b89d35e
Nathan Tonani <nathantonani@gmail.com>
2017-02-06 13:00:45 -0800

    point to deployed app; removed index file;

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -12,8 +12,6 @@ import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.nathantonani.gcejokes.jokesApi.JokesApi;
 import com.nathantonani.javajokes.JavaJoke;
 
@@ -72,15 +70,15 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    //.setRootUrl("https://android-nano-builditbigger.appspot.com/_ah/api/");
-                     // DEV
+                    .setRootUrl("https://android-nano-builditbigger.appspot.com/_ah/api/");
+                    /* // DEV
                     .setRootUrl("http://10.0.3.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
-                    });
+                    });*/
 
             // end options for devappserver
 
